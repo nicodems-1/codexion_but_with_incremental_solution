cad65d398c61ec4a16b4c5584a06bbeb1ef5c1fa
Nicodeme Verdier <niverdie@z1r10p5.42lyon.fr>
2026-08-04 11:20:48 +0200

    removing the is_taken feature as it is redundant and not a good implementation for my strategy

--- actions_coder.c
+++ actions_coder.c
@@ -6,7 +6,7 @@
 /*   By: niverdie <niverdie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/28 01:44:24 by niverdie          #+#    #+#             */
-/*   Updated: 2026/08/02 13:30:30 by niverdie         ###   ########.fr       */
+/*   Updated: 2026/08/04 11:20:13 by niverdie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,37 +40,20 @@ int	refactor(t_coder *coder)
 	return (0);
 }
 
-int check_dongle_status(t_dongle *dongle)
-{
-	printf("check dongle status: %d\n", dongle->taken);
-	if(dongle->taken == 0)
-	{
-		dongle->taken = 1;
-		return (0);
-	}
-	else
-		return(1);
-}
-
 int	get_dongles(t_coder *coder)
 {
 	if (coder->id % 2 == 0)
 	{
 		pthread_mutex_lock(&coder->left_dongle->dongle_lock);
-		if (check_dongle_status(coder->left_dongle) == 0)
 			print_logs("has taken a dongle", coder);
 		pthread_mutex_lock(&coder->right_dongle->dongle_lock);
-		if (check_dongle_status(coder->right_dongle) == 0)
 			print_logs("has taken a dongle", coder);
 	}
 	else
 	{
 		pthread_mutex_lock(&coder->right_dongle->dongle_lock);
-		if (check_dongle_status(coder->right_dongle) == 0)
 		print_logs("has taken a dongle", coder);
-		coder->right_dongle->taken = 0;
 		pthread_mutex_lock(&coder->left_dongle->dongle_lock);
-		if (check_dongle_status(coder->right_dongle) == 0)
 			print_logs("has taken a dongle", coder);
 	}
 	return (0);
